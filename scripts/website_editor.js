f3af925d120a3131c9ef3f613ef5026409fa89d8
John Emrick <emrickj248@comcast.net>
2019-03-28 20:43:15 -0400

    Update website_editor.js

--- scripts/website_editor.js
+++ scripts/website_editor.js
@@ -326,7 +326,7 @@ function clear_pimage(th) {
 function reset_form() {
    document.getElementById("webdata").reset();
    $("[name^='pimage']").hide();
-   $("[name^='pname']").each(function() {
+   $("#pname1,#pname2,#pname3,#pname4,#pname5,#pname6").each(function() {
       var r = delist(this.value);
       update_state(r,this);
    });
