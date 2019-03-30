1589236e255602563e0f511e146f0b3a85efabf9
John Emrick <emrickj248@comcast.net>
2019-03-30 17:52:34 -0400

    Update website_editor.js

--- scripts/website_editor.js
+++ scripts/website_editor.js
@@ -324,7 +324,7 @@ function clear_pimage(th) {
 }
 
 function reset_form() {
-   document.getElementById("webdata").reset();
+   //document.getElementById("webdata").reset();
    $("[name^='pimage']").hide();
    $("#picon1,#picon2,#picon3,#picon4,#picon5,#picon6").html("");
    $("#pname1,#pname2,#pname3,#pname4,#pname5,#pname6").each(function() {
